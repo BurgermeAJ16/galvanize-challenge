1a13c37042eb921513a5881448ecd824728a779b
Amber Burgermeister <AmBurgermeister@gmail.com>
2020-02-23 21:09:27 -0700

    added commens

--- challenge.js
+++ challenge.js
@@ -5,11 +5,15 @@ let myArray = []
 myArray[0] = "Amber Jean Burgermeister"
 myArray[1] = "green"
 
-//
+// a function intended to split a name by spaces
 function cutName (name){
 	return name.split(" ")
 }
 
+// an empty object to store my info
 let myInfo = {}
 
+// assign key-value pairs to properties
 myInfo.fullName = cutName(myArray[0])
+myInfo.favoriteColor = myArray[1]
+myInfo.github = "BurgermeAJ16"
